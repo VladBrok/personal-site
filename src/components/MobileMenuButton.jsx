2ac61934b86b09ca627bbcef71c4ac5dd666d88c
Vlad Brok <vlad.brok99@gmail.com>
2022-07-11 16:39:38 +0300

    Make menu button fixed

--- src/components/MobileMenuButton.jsx
+++ src/components/MobileMenuButton.jsx
@@ -1,6 +1,5 @@
 export default function MobileMenuButton({ isOpen, ...props }) {
   const label = isOpen ? "Close" : "Open";
-  const correctiveButtonStyle = isOpen ? "absolute right-2 top-2" : "";
   const correctiveBarStyle = isOpen ? "-translate-x-4 opacity-0" : "";
   const correctiveEdgeBarStyle = isOpen
     ? "before:rotate-45 before:translate-y-[13px] after:-rotate-45 after:-translate-y-[13px]"
@@ -8,7 +7,7 @@ export default function MobileMenuButton({ isOpen, ...props }) {
 
   return (
     <button
-      className={`z-50 w-10 h-8 flex flex-col justify-between before:bar ${correctiveEdgeBarStyle} after:bar ${correctiveButtonStyle} sm:hidden`}
+      className={`z-50 fixed right-2 top-2 w-10 h-8 flex flex-col justify-between before:bar ${correctiveEdgeBarStyle} after:bar sm:hidden`}
       aria-label={`${label} menu`}
       {...props}
     >
