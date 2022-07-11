7c1c0adfadf98da60bcb8eb0f83012a3a7023649
Vlad Brok <vlad.brok99@gmail.com>
2022-07-11 21:51:29 +0300

    Increase overall responsiveness

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
 
   return (
     <>
-      <header className="fixed w-full top-0 left-0 p-2 flex justify-between bg-white shadow-md sm:p-4">
+      <header className="fixed w-full top-0 left-0 p-2 flex justify-between bg-white shadow-md sm:p-4 md:p-5">
         <a className="text-2xl" href="/">
           Vlad<span className="text-blue">Brok</span>
         </a>
@@ -69,14 +69,14 @@ export default function App() {
         </nav>
       </header>
 
-      <main className="mx-6">
+      <main className="mx-6 sm:mx-8 md:mx-14 lg:mx-32">
         <section className="min-h-screen flex items-center">
           <Element name="home">
             <h1 className="font-semibold text-4xl my-4">
               <span className="text-gray text-xl font-normal block">
                 Hi, my name is
               </span>{" "}
-              <span className="decoration-blue underline underline-offset-2">
+              <span className="block decoration-blue underline underline-offset-2 md:mb-6 md:mt-2">
                 Vladislav Brakalo
               </span>
             </h1>
@@ -94,37 +94,64 @@ export default function App() {
               Projects
             </h2>
 
-            <div className="flex flex-col items-center max-w-xs m-auto shadow-lg shadow-gray rounded-2xl">
-              <img
-                className="w-full block rounded-t-2xl"
-                src="/public/images/project-placeholder.jpg"
-                alt="This is the project"
-              />
-              <div className="mx-4">
-                <h3 className="font-semibold text-2xl mb-5 mt-6">
-                  This is the project
-                </h3>
-                <p className="text-gray text-lg mb-6">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Quidem molestias modi tempora voluptate reiciendis, voluptatem
-                  vero eum eos accusantium cumque.
-                </p>
-                <div className="flex flex-wrap justify-center mb-6">
-                  <div className="flex items-center text-lg mx-2 my-1">
-                    <i className="devicon-html5-plain colored pr-1 pt-1"></i>
-                    <span>HTML</span>
+            <div className="grid auto-rows-fr gap-4 md:grid-cols-2 xl:grid-cols-3">
+              <div className="flex flex-col items-center max-w-xs m-auto shadow-lg shadow-gray rounded-2xl h-full">
+                <img
+                  className="w-full block rounded-t-2xl"
+                  src="/public/images/project-placeholder.jpg"
+                  alt="This is the project"
+                />
+                <div className="mx-4">
+                  <h3 className="font-semibold text-2xl mb-5 mt-6">
+                    This is the project
+                  </h3>
+                  <p className="text-gray text-lg mb-6">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Quidem molestias modi tempora voluptate reiciendis,
+                    voluptatem vero eum eos accusantium cumque.
+                  </p>
+                  <div className="flex flex-wrap justify-center mb-6">
+                    <div className="flex items-center text-lg mx-2 my-1">
+                      <i className="devicon-html5-plain colored pr-1 pt-1"></i>
+                      <span>HTML</span>
+                    </div>
+                    <div className="flex items-center text-lg mx-2 my-1">
+                      <i className="devicon-css3-plain colored pr-1 pt-1"></i>
+                      <span>CSS</span>
+                    </div>
+                    <div className="flex items-center text-lg mx-2 my-1">
+                      <i className="devicon-javascript-plain colored pr-1 pt-1"></i>
+                      <span>JavaScript</span>
+                    </div>
+                    <div className="flex items-center text-lg mx-2 my-1">
+                      <i className="devicon-react-plain colored pr-1 pt-1"></i>
+                      <span>React</span>
+                    </div>
                   </div>
-                  <div className="flex items-center text-lg mx-2 my-1">
-                    <i className="devicon-css3-plain colored pr-1 pt-1"></i>
-                    <span>CSS</span>
-                  </div>
-                  <div className="flex items-center text-lg mx-2 my-1">
-                    <i className="devicon-javascript-plain colored pr-1 pt-1"></i>
-                    <span>JavaScript</span>
-                  </div>
-                  <div className="flex items-center text-lg mx-2 my-1">
-                    <i className="devicon-react-plain colored pr-1 pt-1"></i>
-                    <span>React</span>
+                </div>
+              </div>
+              <div className="flex flex-col items-center max-w-xs m-auto shadow-lg shadow-gray rounded-2xl h-full">
+                <img
+                  className="w-full block rounded-t-2xl"
+                  src="/public/images/project-placeholder.jpg"
+                  alt="This is the project"
+                />
+                <div className="mx-4 flex flex-col h-full">
+                  <h3 className="font-semibold text-2xl mb-5 mt-6">
+                    Another One
+                  </h3>
+                  <p className="text-gray text-lg mb-6">
+                    Lorem ipsum dolor sit amet.
+                  </p>
+                  <div className="flex flex-wrap justify-center mb-6 mt-auto">
+                    <div className="flex items-center text-lg mx-2 my-1">
+                      <i className="devicon-html5-plain colored pr-1 pt-1"></i>
+                      <span>HTML</span>
+                    </div>
+                    <div className="flex items-center text-lg mx-2 my-1">
+                      <i className="devicon-css3-plain colored pr-1 pt-1"></i>
+                      <span>CSS</span>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -138,7 +165,7 @@ export default function App() {
               Contact me
             </h2>
             <form
-              className="my-4 text-left text-gray text-lg"
+              className="my-4 text-left text-gray text-lg md:w-96"
               onSubmit={handleSubmit}
             >
               <div>
@@ -149,7 +176,7 @@ export default function App() {
                   </abbr>
                 </label>
                 <input
-                  className="block rounded-md p-1 border mt-1 mb-4 border-lightgray w-full"
+                  className="block rounded-md p-1 border mt-1 mb-4 border-lightgray w-full md:mb-6"
                   type="text"
                   id="name"
                   name="name"
@@ -163,7 +190,7 @@ export default function App() {
                   </abbr>
                 </label>
                 <input
-                  className="block rounded-md p-1 border mt-1 mb-4 border-lightgray w-full"
+                  className="block rounded-md p-1 border mt-1 mb-4 border-lightgray w-full md:mb-6"
                   type="email"
                   id="email"
                   name="email"
@@ -177,7 +204,7 @@ export default function App() {
                   </abbr>
                 </label>
                 <textarea
-                  className="block rounded-md p-1 border mt-1 mb-4 border-lightgray w-full"
+                  className="block rounded-md p-1 border mt-1 mb-4 border-lightgray w-full md:mb-6"
                   name="msg"
                   id="msg"
                   cols="30"
@@ -185,7 +212,7 @@ export default function App() {
                 ></textarea>
               </div>
               <div>
-                <button className="block w-full px-2 py-1 text-white bg-blue rounded-md text-center my-6 hover:[filter:brightness(110%)]">
+                <button className="block w-full px-2 py-1 text-white bg-blue rounded-md text-center my-6 hover:[filter:brightness(110%)] md:my-10">
                   Send Message
                 </button>
               </div>
@@ -194,7 +221,7 @@ export default function App() {
         </section>
       </main>
 
-      <footer className="border-t-[2px] border-gray text-center p-6 mt-6">
+      <footer className="border-t-[2px] border-gray text-center p-6 mt-6 md:p-10">
         {/* dup: devicon container ?*/}
         <div className="flex justify-center">
           {/* dup: devicon ? */}
