2aab44d2290ca2f700bf00886f3bc9dccb26e5a2
Vlad Brok <vlad.brok99@gmail.com>
2022-07-11 12:47:58 +0300

    Add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,62 @@
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
+
 export default function App() {
-  return <h1 className="shadow-md text-sm">Hello</h1>;
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  function handleCloseMenuClick() {
+    setMenuIsOpen(false);
+  }
+
+  function handleOpenMenuClick() {
+    setMenuIsOpen(true);
+  }
+
+  return (
+    <>
+      <header>
+        <a href="/">VladBrok</a>
+
+        <button
+          className="absolute right-2 top-2 text-4xl text-black"
+          onClick={handleOpenMenuClick}
+        >
+          <FaBars />
+        </button>
+
+        {menuIsOpen && (
+          <nav className="fixed top-0 left-0 z-40 w-screen h-screen flex flex-col items-center justify-center text-center bg-white text-gray">
+            <button
+              className="absolute right-2 top-2 text-5xl text-black"
+              onClick={handleCloseMenuClick}
+            >
+              <MdClose />
+            </button>
+            <ul>
+              <li>
+                <a className="text-3xl p-4 inline-block hover:underline hover:cursor-pointer">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className="text-3xl p-4 inline-block hover:underline hover:cursor-pointer">
+                  Projects
+                </a>
+              </li>
+              <li>
+                <a className="text-3xl p-4 inline-block hover:underline hover:cursor-pointer">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </header>
+
+      <main></main>
+
+      <footer></footer>
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.jsx"],
   theme: {
+    colors: {
+      lightblue: "#00C6C2",
+      white: "#FFF",
+      gray: "#4F4E51",
+      black: "#000",
+    },
     extend: {},
   },
   plugins: [],
