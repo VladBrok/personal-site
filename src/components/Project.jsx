7e1c65d5c74d261bfb9e934c6dd6d0b1c62c105e
Vlad Brok <vlad.brok99@gmail.com>
2022-07-13 09:30:35 +0300

    Adjust font size and project padding

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,17 +10,13 @@ export default function Project({
     <div className="max-w-[20rem] xsm:w-[20rem] m-auto h-full [perspective:1000px] group">
       <div className="flex flex-col items-center w-full h-full [transition:transform_1s] [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)] shadow-around shadow-lightgray dark:shadow-gray rounded-2xl">
         <div className="flex flex-col items-center w-full h-full [backface-visibility:hidden]">
-          <img
-            className="w-full block rounded-t-2xl"
-            src={imageUrl}
-            alt=""
-          />
+          <img className="w-full block rounded-t-2xl" src={imageUrl} alt="" />
           <div className="px-4 flex flex-col w-full h-full">
-            <h3 className="font-semibold text-min-lg mb-6 mt-6 text-contrast">
+            <h3 className="font-semibold text-min-lg my-4 text-contrast sm:my-6">
               {title}
             </h3>
             <p className="mb-6">{description}</p>
-            <div className="flex flex-wrap justify-center mb-6 mt-auto text-contrast">
+            <div className="flex flex-wrap justify-center mb-4 mt-auto text-contrast sm:mb-6">
               {children}
             </div>
           </div>
