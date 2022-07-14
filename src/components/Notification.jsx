451f49c6a4d15670f0386dba130cce9511bd4921
Vlad Brok <vlad.brok99@gmail.com>
2022-07-14 10:47:06 +0300

    Fix notification styles
    Translation was too big

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,11 +2,14 @@ import { AiOutlineClose } from "react-icons/ai";
 
 export default function Notification({ isActive, isError, onClose, children }) {
   const colors = isError ? "bg-red shadow-red" : "bg-blue shadow-blue";
-  const visibility = isActive ? "visible -translate-y-full" : "invisible";
+  const visibility = isActive
+    ? "visible -translate-y-14"
+    : "invisible translate-y-full";
+  console.log("notifi", isActive);
 
   return (
     <div
-      className={`fixed z-50 -translate-x-1/2 bottom-0 translate-y-full rounded-xl left-1/2 flex items-center justify-center text-center font-semibold w-[min(70%,25rem)] py-4 px-8 [transition:visibility_0.6s,transform_0.6s] text-white dark:text-light min-h-14 shadow-sm ${colors} ${visibility}`}
+      className={`fixed z-50 -translate-x-1/2 bottom-0 rounded-xl left-1/2 flex items-center justify-center text-center font-semibold w-[min(70%,25rem)] py-4 px-8 [transition:visibility_0.6s,transform_0.6s] text-white dark:text-light min-h-14 shadow-sm ${colors} ${visibility}`}
     >
       {children}
       <button
