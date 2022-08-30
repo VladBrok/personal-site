cf28644c20db2925b41ce2458273ead1b95dc8fc
Vlad Brok <vlad.brok99@gmail.com>
2022-08-30 23:40:13 +0300

    Remove pt from the Technology component

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -3,11 +3,7 @@ import TechnologyIcon from "./TechnologyIcon";
 export default function Technology({ id, name, isColored }) {
   return (
     <div className="flex-centered">
-      <TechnologyIcon
-        id={id}
-        className="pr-2 pt-[0.15rem]"
-        isColored={isColored}
-      />
+      <TechnologyIcon id={id} className="pr-2" isColored={isColored} />
       <span>{name}</span>
     </div>
   );
